c98ef6ae695cbc68cc552f3b9f923a60dcaf86f4
solident <113713467+solident@users.noreply.github.com>
2023-01-11 21:08:30 -0600

    features added needs final look

---
+++ Starter_Code/StarterCode/static/js/app.js
@@ -0,0 +1,85 @@
+
+function buildCharts(sample) {
+
+    // variable allocation with d3
+    d3.json("samples.json").then((data) => {
+      var samples= data.samples;
+      var resultsarray= samples.filter(sampleobject => 
+          sampleobject.id == sample);
+      var result= resultsarray[0]
+    
+      var ids = result.otu_ids;
+      var labels = result.otu_labels;
+      var values = result.sample_values;
+
+//Bubble
+var LayoutBubble = {
+    margin: { t: 1 },
+    xaxis: { title: "OTU ID" },
+    hovermode: "closest",
+    };
+
+    var DataBubble = [ 
+    {
+      x: ids,
+      y: values,
+      text: labels,
+      mode: "markers",
+      marker: {
+        color: ids,
+        size: values,
+        }
+    }
+  ];
+
+  Plotly.newPlot("bubble", DataBubble, LayoutBubble);
+
+//Bar
+var bar_data =[
+    {
+      y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+      x:values.slice(0,10).reverse(),
+      text:labels.slice(0,10).reverse(),
+      type:"bar",
+      orientation:"h"
+
+    }
+  ];
+
+  var barLayout = {
+    title: "Top 10 OTUs Found",
+    margin: { t: 30, l: 150 }
+  };
+
+  Plotly.newPlot("bar", bar_data, barLayout);
+});
+}
+//
+//Dropdown
+function init() {
+    var selector = d3.select("#selDataset");
+
+//fill options
+d3.json("samples.json").then((data) => {
+  var sampleNames = data.names;
+  sampleNames.forEach((sample) => {
+    selector
+      .append("option")
+      .text(sample)
+      .property("value", sample);
+  });
+
+  //sample list to build initial plots
+  const firstSample = sampleNames[0];
+  buildCharts(firstSample);
+  buildMetadata(firstSample);
+});
+}
+
+function optionChanged(newSample) {
+//fetch new data each time
+buildCharts(newSample);
+buildMetadata(newSample);
+}
+init();
+
